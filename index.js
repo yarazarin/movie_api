c8266a696146cd86f4a20138a02ac993df2d0eb6
yara <uraeel@gmail.com>
2023-04-28 13:49:54 -0400

    added Heroko settenigs

--- index.js
+++ index.js
@@ -278,6 +278,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Error...?؟");
 });
 
-app.listen(8080, () => {
-  console.log("Server listening on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
 });
