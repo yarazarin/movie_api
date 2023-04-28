88bf9ca60cdbdfc292bc564358c389d0e3a23074
yara <uraeel@gmail.com>
2023-04-28 19:16:09 -0400

    passport.authenticate jwt session: false added to users

--- index.js
+++ index.js
@@ -58,16 +58,20 @@ app.get("/", (req, res) => {
 //USERS PARTS
 
 //GET USERS
-app.get("/users", (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.find()
+      .then((users) => {
+        res.status(201).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //GET USER
 app.get("/users/:Username", (req, res) => {
